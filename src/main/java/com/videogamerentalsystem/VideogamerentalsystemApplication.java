21ebc60f53dd02ec435aa6bf0640598440d52ba7
ruben-furlan <rubenfurlan@gmail.com>
2024-03-28 14:01:25 -0300

    Initial commit

---
+++ src/main/java/com/videogamerentalsystem/VideogamerentalsystemApplication.java
@@ -0,0 +1,13 @@
+package com.videogamerentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamerentalsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogamerentalsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/videogamerentalsystem/VideogamerentalsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videogamerentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogamerentalsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
